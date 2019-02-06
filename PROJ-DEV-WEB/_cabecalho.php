9f9c204cd55a28f438bd8f770f37c75084ce1fb6
andrecode0 <47373002+andrecode0@users.noreply.github.com>
2019-02-05 22:57:49 -0200

    Add files via upload

---
+++ PROJ-DEV-WEB/_cabecalho.php
@@ -0,0 +1,26 @@
+<?php
+
+    //CABEÇALHO
+    //=========
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PROJ WEB DEV</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="inc/main.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+
+
+
+</head>
+<body>
---
+++ PROJ-DEV-WEB/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    // INDEX 
+    //======
+
+    include_once('_cabecalho.php');
+
+    echo '<p><span class="fa fa-times"></span>ICONE<span class="fa fa-times"></span></p>';
+
+    include_once('_rodape.php');
+
+?>
